ed765ef0eec4cc9154031e711b43737e09cd2875
cuongluv92 <cuongluv92@gmail.com>
2026-09-09 16:35:35 +0900

    fix(tango-n3): apply final translation audit overrides

--- lib/data/sample-examples.ts
+++ lib/data/sample-examples.ts
@@ -6,6 +6,7 @@ import { applyTangoN3ContextExampleOverride } from "./tango-n3-example-overrides
 import { applyTangoN3FinalContextOverride } from "./tango-n3-example-overrides-final";
 import { applyTangoN3Final2Override } from "./tango-n3-example-overrides-final2";
 import { applyTangoN3Final3Override } from "./tango-n3-example-overrides-final3";
+import { applyTangoN3Final4Override } from "./tango-n3-example-overrides-final4";
 
 /**
  * Đúng 3 ví dụ / từ (1 = exam, 2 = daily, 3 = business), khớp `vocabId` với
@@ -19,4 +20,5 @@ export const sampleExamples: VocabExample[] = rawSampleExamples
   .map(applyTangoN3ContextExampleOverride)
   .map(applyTangoN3FinalContextOverride)
   .map(applyTangoN3Final2Override)
-  .map(applyTangoN3Final3Override);
+  .map(applyTangoN3Final3Override)
+  .map(applyTangoN3Final4Override);
